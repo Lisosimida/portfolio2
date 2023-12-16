e9d9318527f9c68fc4e7dfb86567dfa1c5f9bbf5
Lisosimida <144149294+Lisosimida@users.noreply.github.com>
2023-12-16 20:05:44 +0800

    achievement & projects

--- components/Project.jsx
+++ components/Project.jsx
@@ -10,39 +10,39 @@ const projectData = [
         gitUrl: "https://github.com/Lisosimida/Staff-Management-System"
 
     },
-    {
-        id: 2,
-        title: "ClubDAO",
-        description: "This is a project.",
-        image: "/projects/clubdao.png",
-        gitUrl: "https://github.com/0xYudhishthra/ClubDAO"
-    },
-    {
-        id: 3,
-        title: "FundChat",
-        description: "This is a project for APUBCC Sparkathon & Solana Hyperdrive.",
-        image: "/projects/FundChat.png",
-        gitUrl: "https://github.com/cheongyeechian/fundchat"
-    },
-    {
-        id: 4,
-        title: "Megamask",
-        description: "This is a project for ETH Online.",
-        image: "/projects/megamask.png",
-        gitUrl: "https://github.com/megamask"
-    }, {
-        id: 5,
-        title: "zkJom",
-        description: "This is a project fro ETHKL.",
-        image: "/projects/zkJom.png",
-        gitUrl: "https://github.com/trlau/zkJ0M"
-    }, {
-        id: 6,
-        title: "Attest Me",
-        description: "This is a project for ETH Global Superhack.",
-        image: "/projects/attestme.png",
-        gitUrl: "https://github.com/0xBenjamintan/attestMe"
-    }, 
+    // {
+    //     id: 2,
+    //     title: "ClubDAO",
+    //     description: "This is a project.",
+    //     image: "/projects/clubdao.png",
+    //     gitUrl: "https://github.com/0xYudhishthra/ClubDAO"
+    // },
+    // {
+    //     id: 3,
+    //     title: "FundChat",
+    //     description: "This is a project for APUBCC Sparkathon & Solana Hyperdrive.",
+    //     image: "/projects/FundChat.png",
+    //     gitUrl: "https://github.com/cheongyeechian/fundchat"
+    // },
+    // {
+    //     id: 4,
+    //     title: "Megamask",
+    //     description: "This is a project for ETH Online.",
+    //     image: "/projects/megamask.png",
+    //     gitUrl: "https://github.com/megamask"
+    // }, {
+    //     id: 5,
+    //     title: "zkJom",
+    //     description: "This is a project fro ETHKL.",
+    //     image: "/projects/zkJom.png",
+    //     gitUrl: "https://github.com/trlau/zkJ0M"
+    // }, {
+    //     id: 6,
+    //     title: "Attest Me",
+    //     description: "This is a project for ETH Global Superhack.",
+    //     image: "/projects/attestme.png",
+    //     gitUrl: "https://github.com/0xBenjamintan/attestMe"
+    // }, 
 
 ];
 
